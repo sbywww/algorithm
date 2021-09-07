481b4db925a8e602e212415353ec79ba1c7b875d
Kim deok ki <9718kim@naver.com>
2021-09-07 19:51:11 +0900

    [mode] pull request test

--- baekjoon/1000/Mark_1000.java
+++ baekjoon/1000/Mark_1000.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class mark_1000 {
+class mark_1000 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
--- baekjoon/15552/mode_15552.java
+++ baekjoon/15552/mode_15552.java
@@ -1,27 +1,27 @@
-//import java.io.*;
-//import java.util.ArrayList;
-//
-//public class mode_15552 {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+import java.io.*;
+import java.util.ArrayList;
+
+public class mode_15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(bufferedReader.readLine());
+
+//        ArrayList arrayList = new ArrayList();
+        for (int i = 0; i < t; i++){
+            int a = Integer.parseInt(bufferedReader.readLine());
+            int b = Integer.parseInt(bufferedReader.readLine());
+//            arrayList.add(a+b);
+
+            bufferedWriter.write(a);
+        }
+
 //        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-//        int t = Integer.parseInt(bufferedReader.readLine());
-//
-////        ArrayList arrayList = new ArrayList();
-//        for (int i = 0; i < t; i++){
-//            int a = Integer.parseInt(bufferedReader.readLine());
-//            int b = Integer.parseInt(bufferedReader.readLine());
-////            arrayList.add(a+b);
-//
-//            bufferedWriter.write(a);
+//        for (int i = 0; i < arrayList.size(); i++){
+//            bufferedWriter.write(arrayList.get(i).toString());
+//            bufferedWriter.newLine();
 //        }
-//
-////        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-////        for (int i = 0; i < arrayList.size(); i++){
-////            bufferedWriter.write(arrayList.get(i).toString());
-////            bufferedWriter.newLine();
-////        }
-//        bufferedWriter.flush();
-//        bufferedWriter.close();
-//    }
-//}
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
