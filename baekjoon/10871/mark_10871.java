bfe7a40781bec4e677d71c6be4323caaf1b6dc3e
soobinyoon <soobin.yoon@unlike.kr>
2021-09-12 21:32:26 +0900

    [mark] 백준 10871번 문제풀이

---
+++ baekjoon/10871/mark_10871.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class mark_10871 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        int[] a = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < X) {
+                bw.write(a[i] + " ");
+            }
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
